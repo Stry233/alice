865704871cd0cbbf2f25bdc28daf3af635dea5bb
Yuetian <yuetian.classic@outlook.com>
2026-04-18 20:13:21 -0400

    chore(android & studio): bump versionName to 0.3 for v0.3 release

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.selkacraft.alice"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
